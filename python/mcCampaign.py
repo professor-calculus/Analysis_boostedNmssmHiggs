7c29ab4ce6857eeade73d7d6ce663e1a4eec72b4
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-11-02 17:15:56 +0000

    updated: but due to worflow will need completely changing soon

--- python/mcCampaign.py
+++ python/mcCampaign.py
@@ -2,20 +2,27 @@ import os
 
 # run with
 # $ python $CMSSW_BASE/src/Analysis/Analysis_boostedNmssmHiggs/python/mcCampaign.py > $CMSSW_BASE/src/Analysis/Analysis_boostedNmssmHiggs/python/mcCampaignCommands.txt
-# coopy and paste the output into madgrpah command line (can do it in one big go)
+# copy and paste the output into madgrpah command line (can do it in one big go)
 
 ### ## # USER INPUTS # ## ###
-projectName = "nmssmCascadeAnalysis_v03"
+### where you wish to store your projects (too big for /users)
+projectPath = "/storage/jt15104/madGraphProjects/nmssmCascadeAnalysis_v01" 
+# projectPath = "/users/jt15104/MG5_aMC_v2_4_2/nmssmCascadeAnalysis_v01" # FILES TOO BIG FOR THIS
+# projectPath = "/afs/cern.ch/work/t/taylor/public/madGraphProjects/nmssmCascadeAnalysis_v01"
+
+### where madgraph is installed
 madGraphPath = "/users/jt15104/MG5_aMC_v2_4_2/"
-# createNewProject = True # True if want to generate the new physics
-createNewProject = False # if using project that already exists
+# madGraphPath = "/afs/cern.ch/user/t/taylor/MG5_aMC_v2_4_3/"
+
+createNewProject = True # True if want to generate the new physics
+# createNewProject = False # if using project that already exists
 # higgsMassScan = [70.0, 80.0, 90.0, 100.0, 120.0, 130.0]
-higgsMassScan = [80.0]
+higgsMassScan = [85.0]
 # susyMassScan = [800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0]
 susyMassScan = [1000.0]
 massRatio = 0.95 # massHiggs / massNLSP
 massSplitting = 1.0 # massNLSP - massHiggs - massLSP
-numberEvents = 1000
+numberEvents = 1
 #############################
 
 if createNewProject:
@@ -24,7 +31,7 @@ if createNewProject:
 	print("generate p p > squark squark;")
 	print("add process p p > squark go;")
 	print("add process p p > go go;")
-	print("output %s%s;" % (madGraphPath,projectName))
+	print("output %s;" % projectPath)
 
 for higgsMass in higgsMassScan:
 	nlspMass = higgsMass / massRatio
@@ -36,23 +43,23 @@ for higgsMass in higgsMassScan:
 		outputName = "mH" + str(higgsMass) + "_mSusy" + str(susyMass) + "_ratio" + str(massRatio) + "_splitting" + str(massSplitting) + "_" + str(numberEvents) + "events"
 		outputName = outputName.replace(".", "p")
 
-		print("launch %s -n %s;" % (projectName, outputName))
+		print("launch %s -n %s;" % (projectPath, outputName))
 		print("pythia=ON")
 		print("delphes=ON")
 		print("done")
-		print("%s%s/Cards/delphes_card_CMS.dat" % (madGraphPath,projectName))
+		print("%s/Cards/delphes_card_CMS.dat" % projectPath)
 		print("%smodels/nmssmCascadeAnalysis/P1.param_card" % madGraphPath)
-		print("set mneu1 %f" % lspMass)
-		print("set mneu2 %f" % nlspMass)
-		print("set mh01 %f" % higgsMass)
-		print("set msd1 %f" % susyMass)
-		print("set msd2 %f" % susyMass)
-		print("set msd4 %f" % susyMass)
-		print("set msd5 %f" % susyMass)
-		print("set msu1 %f" % susyMass)
-		print("set msu2 %f" % susyMass)
-		print("set msu4 %f" % susyMass)
-		print("set msu5 %f" % susyMass)
-		print("set mgo %f" % gluinoMass)
+		print("set mass mneu1 %f" % lspMass)
+		print("set mass mneu2 %f" % nlspMass)
+		print("set mass mh01 %f" % higgsMass)
+		print("set mass msd1 %f" % susyMass)
+		print("set mass msd2 %f" % susyMass)
+		print("set mass msd4 %f" % susyMass)
+		print("set mass msd5 %f" % susyMass)
+		print("set mass msu1 %f" % susyMass)
+		print("set mass msu2 %f" % susyMass)
+		print("set mass msu4 %f" % susyMass)
+		print("set mass msu5 %f" % susyMass)
+		print("set mass mgo %f" % gluinoMass)
 		print("set nevents %d" % numberEvents)
 		print("done")
