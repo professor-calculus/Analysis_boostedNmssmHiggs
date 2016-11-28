c59adbcca39a6219e3c5d3ccdaef4d92b56735df
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-11-28 09:32:35 +0000

    added new script for MG workflow on soolin

---
+++ python/untarMadgraphProject.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import re
+
+#############################
+### run with
+### $ python $CMSSW_BASE/src/Analysis/Analysis_boostedNmssmHiggs/python/untarMadgraphProject.py
+### 
+### untars the MG projects
+#############################
+
+#############################
+#############################
+### ## # USER INPUTS # ## ###
+
+motherDir = "/storage/jt15104/madGraphProjects/nmssmCascadeAnalysis_v01/paramCard_type02/"
+
+#############################
+#############################
+#############################
+
+madGraphProjects = os.popen("ls %s" % motherDir, "r").readlines()
+
+for madGraphProject in madGraphProjects:
+
+	if madGraphProject.rstrip()[-7:] == ".tar.gz":
+		projectToUnzip = os.path.join(motherDir,madGraphProject.rstrip())
+		# print projectToUnzip
+		os.system("tar -xvf %s" % projectToUnzip)
