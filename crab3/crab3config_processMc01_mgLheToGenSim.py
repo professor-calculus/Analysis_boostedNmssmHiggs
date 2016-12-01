2aa9088d6d5a902662b6eef46e50bbead5141dee
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-12-01 11:42:08 +0000

    updated config and info for mc production workflow

--- crab3/crab3config_processMc01_mgLheToGenSim.py
+++ crab3/crab3config_processMc01_mgLheToGenSim.py
@@ -2,14 +2,14 @@ from CRABClient.UserUtilities import config
 import os
 config = config()
 
-# submit, from crab directory, with $ crab submit -c crab3config_MC.py
+# submit, from crab directory, with $ crab submit -c crab3config_processMc01_mgLheToGenSim.py
 # but first...
 
 # 1. ensure that you have unzipped the necessary madGraph files using python/unzipMadgraphLhe.py
 
 # 2. EDIT THE FOLLOWING
 madGraphProjectName = 'mH110p0_mSusy1800p0_ratio0p95_splitting1p0_25000events'
-uniqueName = 'processMc01_nmssmSignalV01_REPOcommitHash'
+partOneUniqueName = 'processMc01_nmssmSignalV01_TEST6_REPO_364add'
 
 # 3. MAKE SURE THAT THE FOLLOWING ARE UP TO DATE
 madGraphProjectStore = '/storage/jt15104/madGraphProjects/nmssmCascadeAnalysis_v01/paramCard_type02/'
@@ -21,30 +21,34 @@ config.Data.totalUnits = 2 # -1 for all
 
 config.Data.splitting = 'EventBased' 
 config.Data.inputDBS = 'global'
-config.Data.publication = False
+config.Data.publication = True # means we can access the data in step 2
+config.Data.outputPrimaryDataset = 'nmssmCascade' 
 
-uniqueNameMGP = uniqueName + "_" + madGraphProjectName # combine info for a truly unique record
-config.General.requestName = uniqueNameMGP # name of the crab job project (eg on dashboard)
-config.Data.outputDatasetTag = uniqueNameMGP # name for storage directory beneath /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/CRAB_PrivateMC/
+partOneUniqueNameMGP = partOneUniqueName + "_" + madGraphProjectName # combine info for a truly unique record (must have less than 100 characters)
+config.General.requestName = partOneUniqueNameMGP # name of the crab job project (eg on dashboard)
+config.Data.outputDatasetTag = partOneUniqueNameMGP # name for storage directory beneath /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/CRAB_PrivateMC/
 
 config.General.workArea = 'crab_projects' # name of directory where crab project info is stored
 config.General.transferOutputs = True
 config.General.transferLogs    = True
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'processMc01_mgLheToGenSim_cfg.py'
-config.JobType.inputFiles = [os.path.join(madGraphProjectStore,madGraphProjectName,pathWithinMadgraphProject)]
+config.JobType.psetName = 'processMc01_mgLheToGenSim_cfg.py' # the CMSSW config file used
+config.JobType.inputFiles = [os.path.join(madGraphProjectStore,madGraphProjectName,pathWithinMadgraphProject)] # copies this file over to the workernode
 
 config.Site.storageSite = 'T2_UK_SGrid_Bristol'
 
 
 ### ## # EXTRA INFO # ## ###
 #
-# this crab job turn MG_LHE files into CMSSW_GENSIM ROOT files
+# this crab job turns MG_LHE files into CMSSW_GENSIM ROOT files
 # part 1/3 of making CMSSW_RECO data from MG output
 #
+# the public location of the ouput will go here:
+# /<outputPrimaryDataset>/taylor-<partOneUniqueNameMGP>-<randomHash>/USER
+#
 # output ROOT files will go here:
-# /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/CRAB_PrivateMC/<uniqueNameMGP>/<dateStamp>/0000/
+# /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/CRAB_PrivateMC/<partOneUniqueNameMGP>/<dateStamp>/0000/
 #
 # the names of the output ROOT files will be:
 # nmssmSignalV01_GENSIM_<jobNum>.root
---
+++ crab3/crab3config_processMc02_genSimToAOD_step1of2.py
@@ -0,0 +1,51 @@
+from CRABClient.UserUtilities import config
+import os
+config = config()
+
+# submit, from crab directory, with $ crab submit -c crab3config_processMc02_genSimToAOD_step1of2.py
+# but first...
+
+# 1. EDIT THE FOLLOWING #
+madGraphProjectName = 'mH110p0_mSusy1800p0_ratio0p95_splitting1p0_25000events'
+partTwoUniqueName = 'processMc02_nmssmSignalV01_REPOcommitHash_t2'
+# partOneUniqueName = 'processMc01_nmssmSignalV01_REPOcommitHash' # for the input dataset MIGHT BE MORE COMPLEX!!!
+
+
+partTwoUniqueNameMGP = partTwoUniqueName + "_" + madGraphProjectName # combine info for a truly unique record
+# partOneUniqueNameMGP = partOneUniqueName + "_" + madGraphProjectName
+
+config.General.requestName = partTwoUniqueNameMGP # name of the crab job project (eg on dashboard)
+config.Data.outputDatasetTag = partTwoUniqueNameMGP # name for storage directory beneath /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/<dataset>/
+
+config.General.workArea = 'crab_projects' # name of directory where crab project info is stored
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'processMc02_genSimToAOD_step1of2_cfg.py'
+
+config.Data.inputDataset = '/nmssmCascade/taylor-processMc01_nmssmSignalV01_TEST6_REPO_364add_mH110p0_mSusy1800p0_ratio0p95_splitting1p0_25000events-7e7c7957f1406fa45f48b5ed5bb25728/USER' # will need to make more general
+config.Data.unitsPerJob = 1 # to correspond to a single unit output in partOne of the process
+config.Data.totalUnits = -1 # '-1' does them all
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.publication = True
+
+config.Site.storageSite = 'T2_UK_SGrid_Bristol'
+
+
+### ## # EXTRA INFO # ## ###
+#
+# this crab job turns CMSSW_GENSIM files into INTERMEDIATE2AOD ROOT files
+# part 2/3 of making CMSSW_RECO data from MG output
+#
+# the public location of the ouput will go here:
+# /<outputPrimaryDatasetFROMPART1?>/taylor-<partTwoUniqueNameMGP>-<randomHash>/USER
+#
+# output ROOT files will go here:
+# /hdfs/dpm/phy.bris.ac.uk/home/cms/store/user/taylor/CRAB_PrivateMC/<partTwoUniqueNameMGP>/<dateStamp>/0000/
+#
+# the names of the output ROOT files will be:
+# nmssmSignalV01_AODSIMstep1of2_<jobNum>.root
+# as stated by the config file processMc01_mgLheToGenSim_cfg.py
+############################
