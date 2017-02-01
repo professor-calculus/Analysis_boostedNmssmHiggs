28172ecc14084f0fb846bc5199ec7c49de6c902d
Joseph Taylor <joseph.ross.taylor@cern.ch>
2017-02-01 15:42:39 +0000

    updated to new times

--- batch/submit_htcondorExeJob.py
+++ batch/submit_htcondorExeJob.py
@@ -15,15 +15,15 @@ from htcondorExeJob import *
 ### ## # USER INPUT # ## ###
 ############################
 ############################
-# executable = "tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/DoubleBTaggerEfficiencyStudies/DoubleBTaggerEfficiencyStudies" # wrt CMSSW location
-# code = "src/Analysis/Analysis_boostedNmssmHiggs/bin/DoubleBTaggerEfficiencyStudies.cpp" # wrt CMSSW location
-# inputMotherDir = "/hdfs/user/jt15104/Analysis_boostedNmssmHiggs/patTuples/CMSSW_8_0_21/signalSamples/nmssmSignalCascadeV05_13TeV_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/nmssmSignalCascadeV05_13TeV_patTupleAddBTag_ed8021v1_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/" # should be on hdfs
-# logLocation = "/storage/jt15104/jobLog_Analysis_boostedNmssmHiggs/DoubleBTaggerEfficiencyStudies/"
-
-executable = "tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW/McSignalStudiesCMSSW" # wrt CMSSW location
-code = "src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW.cpp" # wrt CMSSW location
+executable = "tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/DoubleBTaggerEfficiencyStudies/DoubleBTaggerEfficiencyStudies" # wrt CMSSW location
+code = "src/Analysis/Analysis_boostedNmssmHiggs/bin/DoubleBTaggerEfficiencyStudies.cpp" # wrt CMSSW location
 inputMotherDir = "/hdfs/user/jt15104/Analysis_boostedNmssmHiggs/patTuples/CMSSW_8_0_21/signalSamples/nmssmSignalCascadeV05_13TeV_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/nmssmSignalCascadeV05_13TeV_patTupleAddBTag_ed8021v1_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/" # should be on hdfs
-logLocation = "/storage/jt15104/jobLog_Analysis_boostedNmssmHiggs/McSignalStudiesCMSSW/"
+logLocation = "/storage/jt15104/jobLog_Analysis_boostedNmssmHiggs/DoubleBTaggerEfficiencyStudies/"
+
+# executable = "tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW/McSignalStudiesCMSSW" # wrt CMSSW location
+# code = "src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW.cpp" # wrt CMSSW location
+# inputMotherDir = "/hdfs/user/jt15104/Analysis_boostedNmssmHiggs/patTuples/CMSSW_8_0_21/signalSamples/nmssmSignalCascadeV05_13TeV_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/nmssmSignalCascadeV05_13TeV_patTupleAddBTag_ed8021v1_mH70p0_mSusy1000p0_ratio0p99_splitting0p5/" # should be on hdfs
+# logLocation = "/storage/jt15104/jobLog_Analysis_boostedNmssmHiggs/McSignalStudiesCMSSW/"
 
 
 # Option A: INPUT FILES ONE AT A TIME BY HAND
