fad92d828382a579289bdce2d06065c9ec848668
Joseph Taylor <joseph.ross.taylor@cern.ch>
2017-03-29 10:22:51 +0100

    put in friendly working state

--- bin/interactiveSubmitBin.py
+++ bin/interactiveSubmitBin.py
@@ -13,19 +13,18 @@ import os
 ### ## # USER INPUTS # ## ###
 
 # the executable you wish to use
-# executable = "$CMSSW_BASE/tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW/McSignalStudiesCMSSW"
+executable = "$CMSSW_BASE/tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/McSignalStudiesCMSSW/McSignalStudiesCMSSW"
 # executable = "$CMSSW_BASE/tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/DoubleBTaggerEfficiencyStudies/DoubleBTaggerEfficiencyStudies"
-executable = "$CMSSW_BASE/tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/InvestigateEventSelection/InvestigateEventSelection"
+# executable = "$CMSSW_BASE/tmp/slc6_amd64_gcc530/src/Analysis/Analysis_boostedNmssmHiggs/bin/InvestigateEventSelection/InvestigateEventSelection"
 
 # input files to run over (the equiv of what you would get back form 'ls', thus can use wildcards (*,?) and multiple arguments)
-filesToUse = "/hdfs/user/jt15104/Analysis_boostedNmssmHiggs/patTuples/CMSSW_8_0_21/dummyBackground/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/qcdSpring16_CMSSW8021_90f89e0426/bTagPatTuple_*.root"
+filesToUse = "/hdfs/user/jt15104/Analysis_boostedNmssmHiggs/patTuples/CMSSW_8_0_21/signalSamples/nmssmSignalCascadeV05_13TeV_mH70p0_mSusy1200p0_ratio0p99_splitting0p1/nmssmSignalCascadeV05_13TeV_patTupleAddBTag_ed8021v4_mH70p0_mSusy1200p0_ratio0p99_splitting0p1/bTagPatTuple_*.root"
 
 # the NEW output DIR & file in which you wish to create to hold the output
-# outputFile = "$CMSSW_BASE/src/Analysis/Analysis_boostedNmssmHiggs/testingScript/histos.root"
-outputFile = "/users/jt15104/local_Analysis_boostedNmssmHiggs/output_InvestigateEventSelection/100K_events/dummyBackground/CMSSW_8_0_21/qcd/histos.root"
+outputFile = "/users/jt15104/testing/histos.root"
 
 # max number of events to run over (-1 does them all)
-maxevents = 100000
+maxevents = -1
 
 #############################
 ##########################################################
